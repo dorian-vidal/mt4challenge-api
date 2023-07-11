302d4e21fd6158ed7936a4c5472b8b0552b0d108
Djibril Bathily <bathilydbl@gmail.com>
2023-07-11 13:28:08 +0200

    add new promo

--- src/controller/back-office/back-office-promo.controller.ts
+++ src/controller/back-office/back-office-promo.controller.ts
@@ -1,9 +1,11 @@
-import { ApiBearerAuth, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiBody, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { BackOfficePromoService } from 'src/service/back-office-promo.service';
-import { Controller, Inject, Get } from '@nestjs/common';
-import { PromoDto } from 'src/dto/bo/promo.score';
+import { Controller, Inject, Get, Post, Body } from '@nestjs/common';
+import { PromoWithStudentsDto } from 'src/dto/bo/promo-with-students.dto';
+import { PromoDto } from 'src/dto/bo/promo.dto';
 import GeneralEnum from 'src/enum/general.enum';
 import { Logger } from 'winston';
+import { GenericResponseDto } from 'src/dto/generic-response.dto';
 
 
 
@@ -19,11 +21,25 @@ export class BackOfficePromoController {
   @ApiOperation({
     summary: 'Get promo infos such as name, first slug, and students',
   })
-  @ApiResponse({ status: 200, type: PromoDto , isArray: true })
+  @ApiResponse({ status: 200, type: PromoWithStudentsDto , isArray: true })
   @ApiResponse({ status: 401, description: GeneralEnum.NOT_AUTHORIZED })
   @ApiBearerAuth()
-  public async findAll(): Promise<PromoDto[]> {
+  public async findAll(): Promise<PromoWithStudentsDto[]> {
     this.logger.info('HTTP Handling findAll promo');
     return this.backOfficePromoService.findAll();
   }
+
+  @Post('/')
+    @ApiOperation({
+        summary: 'Create a new promo',
+    })
+    @ApiResponse({ status: 200, type: GenericResponseDto })
+    @ApiResponse({ status: 401, description: GeneralEnum.NOT_AUTHORIZED })
+    @ApiBody({ type: PromoDto })
+    @ApiBearerAuth()
+    public async create(@Body() body: PromoDto): Promise<GenericResponseDto> {
+        this.logger.info('HTTP Handling new promo creation');
+        return this.backOfficePromoService.create(body).then(() => GenericResponseDto.ok());
+    }
+
 }
--- src/repository/promo.repository.ts
+++ src/repository/promo.repository.ts
@@ -1,12 +1,13 @@
 import { CustomRepository } from '../decorator/typeorm-ex.decorator';
 import { Repository } from 'typeorm';
 import { PromoEntity } from '../entity/promo.entity';
-import { PromoDto } from 'src/dto/bo/promo.score';
+import { PromoWithStudentsDto } from 'src/dto/bo/promo-with-students.dto';
+import { PromoDto } from 'src/dto/bo/promo.dto';
 
 @CustomRepository(PromoEntity)
 export class PromoRepository extends Repository<PromoEntity> {
 
-  public async findAll(): Promise<PromoDto[]> {
+  public async findAll(): Promise<PromoWithStudentsDto[]> {
     return await this.query(
       `
       SELECT p.name, p.slug, COUNT(a.id) AS students_count
@@ -28,5 +29,15 @@ export class PromoRepository extends Repository<PromoEntity> {
       [slug],
     );
     return result[0];
+  }
+
+  public async createPromo(name: string, slug: string): Promise<void> {
+    await this.query(
+      `
+      INSERT INTO promo (name, slug)
+      VALUES ($1, $2)
+    `,
+      [name, slug],
+    );
   }
 }
